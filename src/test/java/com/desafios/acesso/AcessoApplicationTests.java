5b5551c6083b61584a5f05698cb014db18ed005e
Eric <ericsouza94895@gmail.com>
2025-03-29 23:55:03 -0300

    initial commit

---
+++ src/test/java/com/desafios/acesso/AcessoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafios.acesso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcessoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
