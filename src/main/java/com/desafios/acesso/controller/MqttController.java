771d92b9e305b8e28b5748e39fab83b0c8d205cb
Eric <ericsouza94895@gmail.com>
2025-03-31 23:58:14 -0300

    feat: add mqtt controller

---
+++ src/main/java/com/desafios/acesso/controller/MqttController.java
@@ -0,0 +1,29 @@
+package com.desafios.acesso.controller;
+
+import com.desafios.acesso.service.ClienteMQTT;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/mqtt")
+public class MqttController {
+
+    private final ClienteMQTT clienteMQTT;
+
+    public MqttController(ClienteMQTT clienteMQTT) {
+        this.clienteMQTT = clienteMQTT;
+    }
+
+    @PostMapping("/publicar")
+    public String publicar(@RequestParam String topico, @RequestParam String mensagem) {
+        clienteMQTT.publicarMensagem(topico, mensagem);
+        return "Mensagem publicada no tópico " + topico;
+    }
+
+    @GetMapping("/assinar")
+    public String assinar(@RequestParam String topico) {
+        clienteMQTT.assinarTopicoEIniciarEscuta(topico, mensagem -> {
+            System.out.println("Mensagem Recebida via API: " + mensagem);
+        });
+        return "Inscrito no tópico " + topico;
+    }
+}
