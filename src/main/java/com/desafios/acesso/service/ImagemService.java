d9597a4df04ecf14bf97b9b3ab69da9b1263e8cd
Eric <ericsouza94895@gmail.com>
2025-04-11 23:47:00 -0300

    feat: add the class with the methods to manipulate images in the project

---
+++ src/main/java/com/desafios/acesso/service/ImagemService.java
@@ -0,0 +1,34 @@
+package com.desafios.acesso.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@Service
+public class ImagemService {
+
+    @Value("${file.upload-dir}")
+    private String uploadDir;
+
+    public String storeFile(MultipartFile file) throws IOException {
+        String fileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
+
+        Path targetLocation = Paths.get(uploadDir).toAbsolutePath().normalize().resolve(fileName);
+
+        Files.createDirectories(targetLocation.getParent());
+
+        Files.copy(file.getInputStream(), targetLocation);
+
+        return uploadDir+"/" + fileName;
+    }
+
+    public Path loadFile(String fileName) {
+        return Paths.get(uploadDir).resolve(fileName).normalize();
+    }
+}
